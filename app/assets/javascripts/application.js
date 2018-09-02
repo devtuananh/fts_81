527266ff94efce5e76785ecfd50badf54b852613
hang <hangtran9397@gmail.com>
2018-09-02 21:30:03 +0700

    create static pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
